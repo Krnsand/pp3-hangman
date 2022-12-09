ffa3c74694aa83e3822c69953d03294f82361d36
Krnsand <krnsandin@gmail.com>
2022-12-09 16:26:20 +0000

    Add rules to game

--- run.py
+++ run.py
@@ -31,7 +31,7 @@ class TextColors:
 
 def welcome_to_game():
     """
-    Welcome user to game and ask for their name
+    Welcome user to game and ask for their name. Show logo
     """
     print("{}    {}    {}{}     {}    {}    {}}}}}    {}      {}    {}{}     {}    {}")
     print("{}    {}   {}  {}    {}}}  {}   {}    {}   {}}}  {{{}   {}  {}    {}}}  {}")
@@ -42,21 +42,44 @@ def welcome_to_game():
     username = " "
     while True:
         username = input("Welcome! Please enter your Name: \n")
- 
+
         if username.isalnum() is not True:
             print("Error: Letters and numbers only.")
-            #continue
+
         else:
             print(f"Hi {username}, You have up to 8 guesses to guess the Word.")
             input("When you are ready to play, press 1 to start \n")
             return username
             break
- 
+
+
+def start_game():
+    """
+    Start the game or view the rules of the game
+    """
+
+    if 
+
     print("Press" + TextColors.GREEN + "1" + TextColors.WHITE +
          "to start game")
     print("Press" + TextColors.GREEN + "2" + TextColors.WHITE +
          "to view game rules")
 
 
+def game_rules():
+    """
+    Define the rules of the game
+    """
+    print(
+        """
+        Guess a letter for the random word.
+        Is your letter in the word? Great! You continue without
+        losing a life. Is it not? Sorry! You lose a life. When your 
+        lives reach 0 without having guessed the word correctly, it
+        is game over.
+        """
+            )
+
 welcome_to_game()
+game_rules()
 
