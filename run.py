a5142a62172c29d2c401460a887d3545f84a0b7d
Krnsand <krnsandin@gmail.com>
2022-12-10 14:17:12 +0000

    Add random word generator, begin start game func

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 import random
+import string
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -16,6 +17,10 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('hangman')
+
+scoreboard = SHEET.worksheet("scores")
+#data = scores.get_all_values()
+score = 0
 
 
 class TextColors:
@@ -41,29 +46,60 @@ def welcome_to_game():
 
     username = " "
     while True:
-        username = input("Welcome! Please enter your Name: \n")
+        username = input("Welcome! Please enter your name: \n")
 
         if username.isalnum() is not True:
-            print("Error: Letters and numbers only.")
+            print("Error: Letters and numbers only. \n")
 
         else:
             print(f"Hi {username}, You have up to 8 guesses to guess the Word.")
-            input("When you are ready to play, press 1 to start \n")
+            input("When ready to play, press Enter. \n")
             return username
             break
+
+
+def get_random_word():
+    """
+    Gets a random word from word.txt for each round
+    """
+    random_word = random.choice(open("words.txt", "r").read().split('\n'))
+    return random_word.upper()
 
 
 def start_game():
     """
     Start the game or view the rules of the game
     """
+    lives = 8
+    guesses = []
+    complete = False
+    word_template = "_" * len(word)
+    print(f" Lives: {lives}\n")
+    print(f" The word to guess: " + " ".join(word_template) + "\n")
 
-    if 
+    word = get_valid_word(words)
+    word_letters = set(word) #letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set() # what the user has guessed
 
-    print("Press" + TextColors.GREEN + "1" + TextColors.WHITE +
-         "to start game")
-    print("Press" + TextColors.GREEN + "2" + TextColors.WHITE +
-         "to view game rules")
+    # getting user input
+    user_letter = input("Guess a letter: ").upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in used_letters:
+            word_letters.remove(used_letters)
+    
+    elif user_letter in used_letters:
+        print("You have already guessed that letter. Please guess again.")
+
+    else:
+        print("Invalid character. Please choose a letter.")
+
+
+welcome_to_game()
+get_random_word()
+start_game()
+#game_rules()
 
 
 def game_rules():
@@ -79,7 +115,3 @@ def game_rules():
         is game over.
         """
             )
-
-welcome_to_game()
-game_rules()
-
