01e49e91b4d168c5623a48aaee11549fd34e565c
Krnsand <krnsandin@gmail.com>
2022-12-13 12:22:06 +0000

    Fix bugs: lives not printed, guess not recognize letters in word, prints all words at end

--- run.py
+++ run.py
@@ -83,11 +83,11 @@ def play_game(word):
             else:
                 print(f"Good job, {guess} is in the word!")
                 guessed_letters.append(guess)
-                word_as_list = list(word_completion)
+                word_completion_list = list(word_completion)
                 indices = [i for i, letter in enumerate(word) if letter == guess]
                 for index in indices:
-                    word_as_list[index] = guess
-                word_completion = "".join(word_as_list)
+                    word_completion_list[index] = guess
+                word_completion = "".join(word_completion_list)
                 if "_" not in word_completion:
                     guessed = True
 
